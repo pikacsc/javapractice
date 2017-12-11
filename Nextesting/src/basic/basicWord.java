cf6de825631487bd7398b10405cd6148db4dc1e5
JasonChoi <ccc11203@gmail.com>
2017-12-11 17:53:12 +0900

    두번째 테스트

---
+++ Nextesting/src/basic/basicWord.java
@@ -0,0 +1,18 @@
+package basic;
+
+public class basicWord {
+	public static void main(String []args) {
+	basicWord bw = new basicWord();
+	int a = 1;
+	bw.breakingWood(a);
+	}
+	public void breakingWood(int a){
+		while(a<10) {
+			a++;
+			System.out.println("나무를" + a + "번찍었습니다");
+	       if(a==10) {
+	    	  System.out.println("나무 넘어 갑니다");
+	       }
+		}
+	}
+}
