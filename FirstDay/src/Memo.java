11040c58ca65ef56fe77dba1ae25daa76a600f34
JasonChoi <ccc11203@gmail.com>
2017-12-11 17:29:37 +0900

    init

---
+++ FirstDay/src/Memo.java
@@ -0,0 +1,15 @@
+
+public class Memo {
+
+	public static void main(String[] args) {
+		System.out.println("수업 첫날");
+		System.out.println("1.jre,jdk 설치");
+		System.out.println("2.환경변수 설정");
+		System.out.println("3.eclipse 설치");
+		System.out.println("4.git 연습");
+		System.out.println("5.새내용 추가");
+		
+		
+	}
+
+}
