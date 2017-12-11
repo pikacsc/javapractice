d44436a4ab25b4a16d73e6149cd3875b7308182e
JasonChoi <ccc11203@gmail.com>
2017-12-11 18:08:48 +0900

    17-12-11 6:08 새내용 추가 삭제함

--- FirstDay/src/Memo.java
+++ FirstDay/src/Memo.java
@@ -7,8 +7,7 @@ public class Memo {
 		System.out.println("2.환경변수 설정");
 		System.out.println("3.eclipse 설치");
 		System.out.println("4.git 연습");
-		System.out.println("5.새내용 추가");
-		
+		//새내용추가 삭제함
 		
 	}
 
