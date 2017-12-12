bb8b13ad143d620ab52bb6a599645374d6f2c0fd
JasonChoi <ccc11203@gmail.com>
2017-12-12 11:32:41 +0900

    주석처리 및 Javadoc

---
+++ Exam_3 Day2/src/ojc/OjcSample.java
@@ -0,0 +1,28 @@
+package ojc;
+
+/** 
+ * 자바초보자용 샘플 클래스
+ * <pre>
+ * <b>History;</b>
+ *    2015.6.1 최초작성
+ *    2015.12.1 메소드 추가
+ * @author 최성찬
+ * @version 1.1 2017.12.12 출력문 추가
+ * 
+ * 
+ */
+
+public class OjcSample {
+	/**
+	 * 
+	 * 메인메소드
+	 * @param args 메인메소드인자
+	 */
+	public static void main(String[] args) {
+			/**
+			 * 출력문
+			 */
+		System.out.println("자바독 주석연습");
+	}
+
+}
---
+++ SecondDay_2/src/Ojcsample/OjcSample.java
@@ -0,0 +1,34 @@
+package Ojcsample;
+
+public class OjcSample {
+
+	public static void main(String[] args) {
+		// int 는 제한된 범위안에서 - + 정수를 담을 수 있다
+		// sum 
+		int sum = 0; //선언문, 로컬변수선언
+		
+		
+		
+		    //for(초기값 ; 제한범위 ; 제한범위까지 하는 동작)
+		for(int i = 1; i<10; i++) {             //반복문
+			sum = sum + i;  //대입문
+			if(sum == 9) { 	
+				System.exit(9);   //제어문 
+			System.out.println("합계는" + sum);//출력문
+			}
+		}
+		/*시작
+		 * 
+		 * 아무거나 적어도 다 주석이 된다.
+		 * 
+		 * 
+		 * 
+		 * 
+		끝까지 주석 처리 */
+//		블럭지정후 ctrl + / 해주면 주석처리가 쉬워짐
+		
+		
+		
+	}
+
+}
