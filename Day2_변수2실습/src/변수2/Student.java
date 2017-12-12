4f9036c6710e3df156d7010fadad6c3a36ac94b5
JasonChoi <ccc11203@gmail.com>
2017-12-12 14:34:31 +0900

    12.12 실습 2

--- Day2_변수2실습/src/변수2/Student.java
+++ Day2_변수2실습/src/변수2/Student.java
@@ -39,21 +39,54 @@ public class Student {
 		 * 
 		 */
 		
+//		
+//		String name = "홍길동";
+//		int age = 28;
+//		float height = 173.9f;
+//		String gender = "남자";
+//		String work = "회사원";
+//		
+//		System.out.println("이름: "+ name);
+//		System.out.println("나이: "+ age);
+//		System.out.println("키: "+ height);
+//		System.out.println("성별: "+ gender);
+//		System.out.println("직업: "+ work);
+//				
 		
-		String name = "홍길동";
-		int age = 28;
-		float height = 173.9f;
-		String gender = "남자";
-		String work = "회사원";
+		/*
+		 * 실습 2
+		 * 
+		 * 문자열 변수 로 지정
+		 * "Hello"
+		 * "Java"
+		 * "Nice to meet you"
+		 * "Bye"
+		 * 
+		 * 
+		 * 출력문에 변수만 사용해서 출력하기
+		 * 1.Hello Java Nice to meet you bye
+		 * 
+		 * 2. Hello
+		 *    Java
+		 *    Nice to meet you
+		 *    Bye
+		 * 
+		 * 
+		 * 
+		 */
 		
-		System.out.println("이름: "+ name);
-		System.out.println("나이: "+ age);
-		System.out.println("키: "+ height);
-		System.out.println("성별: "+ gender);
-		System.out.println("직업: "+ work);
-				
+		String a,b,c,d;
+		a ="Hello ";
+		b ="Java ";
+		c ="Nice to meet you";
+		d ="bye";
 		
-		
+		System.out.println("1."+ a + b + c + d);
+		System.out.println("");
+		System.out.println("2."+a);
+		System.out.println("  "+b);
+		System.out.println("  "+c);
+		System.out.println("  "+d);
 		
 		
 	}
