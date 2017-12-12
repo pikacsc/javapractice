b73117ec76c2946319c4fb2a2dc1736576ef1665
JasonChoi <ccc11203@gmail.com>
2017-12-12 14:22:25 +0900

    12.12일 최종실습 1.변수 선언 및 초기화 2.출력문에 변수 사용할때 "+변수+" 사용하기

---
+++ Day2_변수2실습/src/변수2/Student.java
@@ -0,0 +1,61 @@
+package 변수2;
+
+public class Student {
+
+	public static void main(String[] args) {
+//		int a; 변수 만들기
+//		a = 1;
+//		System.out.println(a);
+//		
+//		a = 10; //a의 값을 10으로 수정, 자동으로 1은 사라짐
+//		
+//		System.out.println(a);
+//		
+//		System.out.println("당신의 동전은 "+a+"원입니다");
+//		
+//		
+//		
+//		String b = "당신의 동전은";
+//		
+//		System.out.println(b);
+//		
+//		b = ""; //초기화
+//		
+//		b = "원 입니다";
+//
+//		System.out.println(a+b);
+//		//문자열 변수에 할당하여 출력
+//		
+		
+		/* 실습
+		 *
+		 * 이름: 홍길동 
+		 * 나이: 28
+		 * 키 : 173.9
+		 * 성별 : 남자
+		 * 직업: 회사원
+		 *
+		 * 왼쪽은 print 오른쪽은 변수로 지정하기
+		 * 
+		 */
+		
+		
+		String name = "홍길동";
+		int age = 28;
+		float height = 173.9f;
+		String gender = "남자";
+		String work = "회사원";
+		
+		System.out.println("이름: "+ name);
+		System.out.println("나이: "+ age);
+		System.out.println("키: "+ height);
+		System.out.println("성별: "+ gender);
+		System.out.println("직업: "+ work);
+				
+		
+		
+		
+		
+	}
+
+}
