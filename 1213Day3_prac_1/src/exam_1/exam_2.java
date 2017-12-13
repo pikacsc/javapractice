c92d598541bea8db4af2099581118844139ce65a
JasonChoi <ccc11203@gmail.com>
2017-12-13 11:12:58 +0900

    변수의 종류들, 데이터 타입별 초기화

---
+++ 1213Day3_prac_1/src/exam_1/exam_2.java
@@ -0,0 +1,20 @@
+package exam_1;
+
+public class exam_2 {
+
+	public static void main(String[]args) {
+		//변수의 초기화:변수에 리터럴을 할당하는것
+		//이하 Local Variable(지역변수)들 
+		boolean b = true; // true || false
+		char c = 'A';
+		String s = "문자열";
+		byte bb = 1;
+		short sh = 2000;
+		int gg = 1000001313;
+		long l = 100;
+		float ff = 100.0f;
+		double dd = 100.0;
+		
+		
+	}
+}
