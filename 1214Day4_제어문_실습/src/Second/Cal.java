b53aebee2b6a51b2c678715980284333e1364d7f
JasonChoi <ccc11203@gmail.com>
2017-12-14 17:49:01 +0900

    12.14 실습2 if문, switch문, scanner 객체로 사칙연산 계산기 만들기

---
+++ 1214Day4_제어문_실습/src/Second/Cal.java
@@ -0,0 +1,80 @@
+package Second;
+
+import java.util.Scanner;
+
+public class Cal {
+
+	public static void main(String[] args) {
+		int a,b;
+		String op;
+		int c;
+		Cal cal = new Cal();
+		Scanner sc = new Scanner(System.in);
+		Scanner sc2 = new Scanner(System.in);
+		System.out.println("숫자 두개를 입력하세요 a b");
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.println("연산자중 하나를 입력하세요 + - * /:");
+		op = sc2.next();
+		System.out.println("구현 방법을 선택하세요 1.if문, 2.switch문");
+		c = sc.nextInt();
+		switch(c) {
+			case 1:
+				cal.ifLogic(op,a,b);
+				break;
+			case 2:
+				cal.ifLogic(op,a,b);
+				break;
+			default:
+				System.out.println("오류:없는 구현방법입니다");
+				break;
+		}
+	}
+	
+	int result;
+	public void ifLogic(String op,int a, int b) {
+		if(op.equals("+")) { 
+			result = a+b;
+			System.out.println("출력: "+a+op+b+"="+result);
+		}
+		else if(op.equals("-")) { 
+			result = a-b;
+			System.out.println("출력: "+a+op+b+"="+result);
+		}
+		else if(op.equals("*")) { 
+			result = a*b;
+			System.out.println("출력: "+a+op+b+"="+result);
+		}
+		else if(op.equals("/")) { 
+			result = a/b;
+			System.out.println("출력: "+a+op+b+"="+result);
+		} else {
+			System.out.println("오류, 없는 연산자입니다");
+		}
+	} 
+	
+	public void switchLogic(String op, int a, int b) {
+		
+		switch(op) {
+		case "+":
+			result = a+b;
+			System.out.println("출력: "+a+op+b+"="+result);
+			break;
+		case "-":
+			result = a-b;
+			System.out.println("출력: "+a+op+b+"="+result);
+			break;
+		case "*":
+			result = a*b;
+			System.out.println("출력: "+a+op+b+"="+result);
+			break;
+		case "/":
+			result = a/b;
+			System.out.println("출력: "+a+op+b+"="+result);
+			break;
+		default :
+			System.out.println("오류, 없는 연산자입니다.");
+			break;
+		}
+	}
+}
---
+++ 1214Day4_제어문_실습/src/Second/ScoreMaker_switch.java
@@ -0,0 +1,40 @@
+package Second;
+
+import java.util.Scanner;
+
+public class ScoreMaker_switch {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int 국어;
+		int 영어;
+		int 수학;
+		String l= "학생의 등급은";
+		
+		System.out.println("국어 영어 수학 점수를 차례대로 띄어쓰기를 이용하여 입력하시오");
+		System.out.println("국어 영어 수학 : ");
+		국어 = sc.nextInt();
+		영어 = sc.nextInt();
+		수학 = sc.nextInt();
+		
+		int score = (국어+영어+수학)/3;
+		String result = 90<=score && 100 >= score? "A" : 80<=score && 90 > score ? "B":70<=score && 80 > score?"C":60<=score && 70 > score?"D":"F";   
+		switch(result) {
+			case "A":
+				 System.out.println(l+" A");
+				 break;
+			case "B":
+				 System.out.println(l+" B");
+				 break;
+			case "C":
+				 System.out.println(l+" C");
+				 break;
+			case "D":
+				 System.out.println(l+" D");
+			default:
+				 System.out.println(l+" F");
+				 break;
+		}
+	}
+
+}
