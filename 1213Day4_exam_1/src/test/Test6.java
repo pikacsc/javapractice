5fbc7432c122c63cd4ef174e62298d8a076e6e65
JasonChoi <ccc11203@gmail.com>
2017-12-14 11:43:59 +0900

    ★응용 삼항 연산자,메소드 생성하고 객체생성하여 나타내기

--- 1213Day4_exam_1/src/test/Test6.java
+++ 1213Day4_exam_1/src/test/Test6.java
@@ -3,6 +3,8 @@ package test;
 import java.util.Scanner;
 
 public class Test6 {
+	String result;
+	
 	public static void main(String[]args) {
 		/*
 		 * 숫자하나늘 입력을 받아서 
@@ -13,15 +15,26 @@ public class Test6 {
 		 * 
 		 */
 		Scanner sc = new Scanner(System.in);
-		String result;
+		Test6 t6 = new Test6();
 		int numb= 0;
 		System.out.print("숫자를 입력하세요:");
 		numb = sc.nextInt();
+		t6.numbCheck(numb);
 		
-		
-		result = (numb%2==0)? "짝입니다 true":"홀입니다 false";
-		
-		System.out.println(result);
+//		result = (numb%2==0)? "짝입니다 true":"홀입니다 false";
+//		
+//      응용↓		
 	}
 
+	
+	private String numbCheck(int numb) {
+	
+		result = (numb%2==0)? (numb>10)?"10보다 큽니다":"10보다 작습니다":(numb>10)?"10보다 큽니다":"10보다 작습니다";
+		
+		
+		System.out.println(result);
+		
+		return result;
+	}
+	
 }
