9575dc5f93bbcb44cd3c68f45115d30e0767e57b
JasonChoi <ccc11203@gmail.com>
2017-12-18 18:20:20 +0900

    ''

--- 1218Day6_반복문_실습/src/second/exam_4.java
+++ 1218Day6_반복문_실습/src/second/exam_4.java
@@ -31,35 +31,34 @@ public class exam_4 {
 	 */
 
 	public static void main(String[] args) {
-		  String stars[] = {"*","**","***","****","*****"};
 
-		  LABEL:
-			  for(int i=0,j=0;i<stars.length;i++) {
-				  System.out.println(stars[i]);
-				  if(i==4) {
-					  System.out.println();
-					  for(;i>=0;i--) {
-						  System.out.println(stars[i]);
-						  if(i==0) {
-							  System.out.println();
-							  break LABEL;
-						  } 
-					  }
-				  }
-		   
-			  }
-		  
-		  LABEL2:
-			  for(int i=0;i<stars.length;i++) {
-				  System.out.printf("%5s \n",stars[i]);
-				  if(i==4) {
-					  System.out.println();
-					  for(;i>=0;i--) {
-						  System.out.printf("%5s \n",stars[i]);
-						  if(i==0) break LABEL2;
-					  }
-				  }
-			  } 
+//		  LABEL:
+//			  for(int i=0,j=0;i<stars.length;i++) {
+//				  System.out.println(stars[i]);
+//				  if(i==4) {
+//					  System.out.println();
+//					  for(;i>=0;i--) {
+//						  System.out.println(stars[i]);
+//						  if(i==0) {
+//							  System.out.println();
+//							  break LABEL;
+//						  } 
+//					  }
+//				  }
+//		   
+//			  }
+//		  
+//		  LABEL2:
+//			  for(int i=0;i<stars.length;i++) {
+//				  System.out.printf("%5s \n",stars[i]);
+//				  if(i==4) {
+//					  System.out.println();
+//					  for(;i>=0;i--) {
+//						  System.out.printf("%5s \n",stars[i]);
+//						  if(i==0) break LABEL2;
+//					  }
+//				  }
+//			  } 
 	}	
 		 
 	
