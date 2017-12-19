1c41725bdffbffe3ad979edbb03118003e6ec63b
JasonChoi <ccc11203@gmail.com>
2017-12-19 10:51:01 +0900

    전날 실습2 for문으로 *그림 그리기

--- 1218Day6_반복문_실습/src/second/exam_4.java
+++ 1218Day6_반복문_실습/src/second/exam_4.java
@@ -59,6 +59,31 @@ public class exam_4 {
 //					  }
 //				  }
 //			  } 
+		
+		//Ç®ÀÌ
+		for(int i=0;i<5;i++) {
+			for(int j=0;j<5;j++) {
+				if(i>j || i==j) {
+				System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+		System.out.println();
+		for(int i=0;i<5;i++) {
+			for(int j=0;j<5;j++) {
+				//System.out.printf("%d%d ",i,j);
+				if(i+j<=4) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		
+		
+		
 	}	
 		 
 	
