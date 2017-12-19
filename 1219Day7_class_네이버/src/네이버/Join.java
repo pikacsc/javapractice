e7a06e1b7e52145ce8a2b818a9d991c94ff811e7
JasonChoi <ccc11203@gmail.com>
2017-12-19 14:21:06 +0900

    메소드 영역,매개변수,리턴

--- 1219Day7_class_네이버/src/네이버/Join.java
+++ 1219Day7_class_네이버/src/네이버/Join.java
@@ -2,6 +2,7 @@ package 네이버;
 
 //회원가입
 public class Join {
+	//필드영역
 	String id;
 	String pw;
 	String name;
@@ -11,4 +12,45 @@ public class Join {
 	short bornday;
 	String email;
 	int phone;
+	
+	
+	//메소드(함수) 영역, 동작하는 영역
+	// 메소드 작성법
+	/*
+	 *                     parameter매개변수
+	 * 접근제어자 리턴자료형 함수명(자료형 변수명){
+	 * 
+	 *  return 리턴자료; 리턴자료는 다른 곳에서 쓸수 있음
+	 * }
+	 * 
+	 *                   parameter매개변수 
+	 * 접근제어자 void 함수명(자료형 변수명){
+	 * 
+	 * 리턴하는게 없음
+	 * }
+	 * 
+	 * 
+	 * 예시
+	 * 
+	 * 
+	 * 
+	 */
+	//출력하기 함수
+	void 출력하기(Join x) {
+		System.out.printf("이름은 %s입니다. \n",x.name);
+		System.out.printf("%d년 %d월 %d일생 입니다 \n",x.bornyear,x.bornMonth,x.bornday);
+		System.out.printf("핸드폰 번호는 0%d \n",x.phone);
+		if(x.gender.equals("남자")) {
+			System.out.print("성별은 남자입니다.");
+		}else {
+			System.out.print("성별은 여자입니다.");
+		}
+		System.out.printf("패스워드는 %s \n",x.pw);
+	}
+	
+	String 가입하기(String a) {
+		System.out.println("매개변수 = "+a);
+		System.out.println("가입하기 함수를 사용하셨습니다");
+		return a; 
+	}
 }
--- 1219Day7_class_네이버/src/네이버/Naver.java
+++ 1219Day7_class_네이버/src/네이버/Naver.java
@@ -5,52 +5,43 @@ import java.util.Scanner;
 public class Naver {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		short menuInput;
 		
 		Naver nav = new Naver();
 		System.out.println("네이버에 오신것을 환영합니다.");
 	
 		
 		Join member = new Join();
-		
-		System.out.print("이름을 입력하세요:");
-		member.name = sc.nextLine();
-		
-		System.out.print("이메일을 입력하세요:");
-		member.email = sc.nextLine();
-		
-		System.out.print("성별을 입력하세요:");
-		member.gender = sc.next();
-		
-		System.out.print("생년 월 일 띄어쓰기를 사용하여 한번에 입력하세요:");
-		member.bornyear = sc.nextInt();
-		member.bornMonth = sc.nextShort();
-		member.bornday = sc.nextShort();
-		
-		System.out.print("휴대폰 번호를 입력하세요:");
-		member.phone = sc.nextInt();
-		
-		System.out.println("비밀번호를 입력하세요:");
-		member.pw = sc.next();
-		
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		nav.printData(member);
+//		
+//		System.out.print("이름을 입력하세요:");
+//		member.name = sc.nextLine();
+//		
+//		System.out.print("이메일을 입력하세요:");
+//		member.email = sc.nextLine();
+//		
+//		System.out.print("성별을 입력하세요:");
+//		member.gender = sc.next();
+//		
+//		System.out.print("생년 월 일 띄어쓰기를 사용하여 한번에 입력하세요:");
+//		member.bornyear = sc.nextInt();
+//		member.bornMonth = sc.nextShort();
+//		member.bornday = sc.nextShort();
+//		
+//		System.out.print("휴대폰 번호를 입력하세요:");
+//		member.phone = sc.nextInt();
+//		
+//		System.out.println("비밀번호를 입력하세요:");
+//		member.pw = sc.next();
+//		
+//		System.out.println();
+//		System.out.println();
+//		System.out.println();
+//		member.출력하기(member);
+		String a = member.가입하기("펜을 던졌습니다");
+		System.out.println("a");
 	}
+	    
 	
-	public void printData(Join x) {
-		System.out.printf("이름은 %s입니다. \n",x.name);
-		System.out.printf("%d년 %d월 %d일생 입니다 \n",x.bornyear,x.bornMonth,x.bornday);
-		System.out.printf("핸드폰 번호는 0%d \n",x.phone);
-		if(x.gender.equals("남자")) {
-			System.out.print("성별은 남자입니다.");
-		}else {
-			System.out.print("성별은 여자입니다.");
-		}
-		System.out.printf("패스워드는 %s \n",x.pw);
-		
-	}
-
+	
+	
 
 }
