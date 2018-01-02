2e05ecf165f4766bfea2828d256e9b78f650222c
JasonChoi <ccc11203@gmail.com>
2018-01-02 13:21:09 +0900

    Vector<Integer>

---
+++ 180102Day15_Vector/src/벡터포인트/PointVectorEx.java
@@ -0,0 +1,32 @@
+package 벡터포인트;
+
+import java.util.Vector;
+
+class Point{
+	private int x, y;
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+		
+	}
+	
+	public String toString() {
+		return "("+ x +"," + y + ")";
+	}
+}
+
+public class PointVectorEx {
+	
+	
+	public static void main(String[] args) {
+		Vector<Point> v = new Vector<Point>();
+		v.add(new Point(2, 3));
+		v.add(new Point(30, -8));
+		
+		for(int i =0;i<v.size();i++) {
+			Point p = v.get(i);
+			System.out.println(p);
+		}
+	}
+
+}
