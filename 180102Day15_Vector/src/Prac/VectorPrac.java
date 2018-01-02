715da71135fe8e518572a11a881f1b81c4d51870
JasonChoi <ccc11203@gmail.com>
2018-01-02 14:29:31 +0900

    Vector<클래스> 벡터명, 응용

---
+++ 180102Day15_Vector/src/Prac/VectorPrac.java
@@ -0,0 +1,42 @@
+package Prac;
+
+import java.util.Scanner;
+import java.util.Vector;
+
+
+class Book {
+
+	public String name; 
+	public String author; 
+	public String publisher; 
+	public int price;
+	
+	public Book(String name,String author,String publisher,int price) {
+		this.name = name;
+		this.author = author;
+		this.publisher = publisher;
+		this.price = price;
+	}
+	
+}
+
+
+public class VectorPrac {
+
+	
+	public static void main(String[] args) {
+		Vector<Book> v2 = new Vector<Book>();
+		
+		v2.add(new Book("난장이가 쏘아올린공","조세희","삼익출판사",500));
+		
+		for(int i=0;i<v2.size();i++) {
+			System.out.println("책이름:"+v2.get(i).name);
+			System.out.println("저자:"+v2.get(i).author);
+			System.out.println("출판사:"+v2.get(i).publisher);
+			System.out.println("가격:"+v2.get(i).price);
+		}
+	}
+	
+	
+
+}
